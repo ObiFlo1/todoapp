fa95fff4e258d1246a2355c1f50eca5332876677
ObiFlo1 <rubflores1000@gmail.com>
2025-08-02 18:36:10 -0700

    add complete task added on screen with nice css look

--- frontEnd/index.js
+++ frontEnd/index.js
@@ -34,10 +34,48 @@ function unpdateTodoList() {
 }
 
 function createTodoItem(todo, todoIndex) {
-  // how do you know you want to add this powerup. 'todo' who are we giving this power up object to ?
+  const todoId = "todo-" + todoIndex;
   const todoLI = document.createElement("li");
   //i bet im going to use the 'todo' task in a userInput(todo) something like that. bc in line 23 in the html file its an <input> tag
   //   todoLI.innerText = todo; // see
   todoLI.className = "todo";
+  todoLI.innerHTML = `
+   <input
+            type="checkbox"
+            id="${todoId}"
+          />
+          <label
+            class="custom-checkbox"
+            for="${todoId}"
+            ><svg
+              fill="transparent"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#e3e3e3"
+            >
+              <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" /></svg
+          ></label>
+          <label
+            for="${todoId}"
+            class="todo-text"
+            >${todo}</label
+          >
+          <button class="delete-button">
+            <svg
+              fill="var(--secondary-color)"
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#e3e3e3"
+            >
+              <path
+                d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"
+              />
+            </svg>
+          </button>
+  `;
   return todoLI;
 }
